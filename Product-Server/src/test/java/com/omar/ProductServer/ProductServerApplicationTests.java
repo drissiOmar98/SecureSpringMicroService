010dc13becc1a0dbf4e4b2667f15465dd578b15c
DrissiOmar <omar.drissi@esprit.tn>
2024-05-20 14:13:05 +0100

    initial commit

---
+++ Product-Server/src/test/java/com/omar/ProductServer/ProductServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.ProductServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
